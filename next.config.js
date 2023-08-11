3403352b3c3106e2e44efd3c7d5dd567fc539d29
Hassan Ali <hassanalimalek9@gmail.com>
2023-08-11 15:35:14 +0500

    config update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     images: {
         domains: ['cdn.sanity.io'],
+        env: {  
+          NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+          NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+          NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+          NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+          NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+          CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+          NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
+          
+         }
       },
 }
 
