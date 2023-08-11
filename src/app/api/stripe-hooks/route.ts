51a88c2fc5370bc60f7cd66694c7c517212b97da
Hassan Ali <hassanalimalek9@gmail.com>
2023-08-11 15:07:33 +0500

    env fix

--- src/app/api/stripe-hooks/route.ts
+++ src/app/api/stripe-hooks/route.ts
@@ -8,22 +8,34 @@ export async function POST(request: NextRequest, response: NextResponse) {
   // Handle the event based on its type
   if (result.type === 'checkout.session.completed') {
     // Payment success event, handle accordingly
-    console.log('process.env.DOMAIN} --->', process.env.DOMAIN);
+    console.log(
+      'process.env.NEXT_PUBLIC_DOMAIN --->',
+      process.env.NEXT_PUBLIC_DOMAIN
+    );
+    console.log('process.env-->', process.env);
+    console.log(
+      'process.env.NEXT_PUBLIC_SANITY_PROJECT_ID',
+      process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+    );
+    console.log('https://dinemarket-kappa.vercel.app/api/stripe-hooks ');
     if (hook_response) {
       try {
-        await fetch(`${process.env.DOMAIN}/api/order`, {
+        await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/order`, {
           method: 'PATCH',
           headers: { 'Content-type': 'application/json' },
           body: JSON.stringify({
             order_id: result.data.object.id,
           }),
         });
-      } catch (error) {
+      } catch (error: any) {
         console.log('Error in hook --->', error);
-        return NextResponse.json({
-          message: 'Unable to complete processing',
-          data: error,
-        });
+        return NextResponse.json(
+          {
+            message: 'Unable to complete processing',
+            data: error?.message,
+          },
+          { status: 400 }
+        );
       }
     }
   }
