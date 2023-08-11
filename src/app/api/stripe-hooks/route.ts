a625d3c274fb95e9e27a1827874164d5f11f102e
Hassan Ali <hassanalimalek9@gmail.com>
2023-08-11 14:24:25 +0500

    temp update

--- src/app/api/stripe-hooks/route.ts
+++ src/app/api/stripe-hooks/route.ts
@@ -1,19 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest, response: NextResponse) {
+  console.log('post request in stripe --hooks');
   const hook_response: any = await request.body;
+  console.log('hook_response --->', hook_response);
   const result = await request.json();
   // Handle the event based on its type
   if (result.type === 'checkout.session.completed') {
     // Payment success event, handle accordingly
+    console.log('process.env.DOMAIN} --->', process.env.DOMAIN);
     if (hook_response) {
-      await fetch(`${process.env.DOMAIN}/api/order`, {
-        method: 'PATCH',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify({
-          order_id: result.data.object.id,
-        }),
-      });
+      try {
+        await fetch(`${process.env.DOMAIN}/api/order`, {
+          method: 'PATCH',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify({
+            order_id: result.data.object.id,
+          }),
+        });
+      } catch (error) {
+        console.log('Error in hook --->', error);
+      }
     }
   }
   return NextResponse.json({
